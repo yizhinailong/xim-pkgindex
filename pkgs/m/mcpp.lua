8159f2ec90275a8da1fc00f95d9ba27f9f0fcdbe
SPeak <speakshen@163.com>
2026-05-24 15:08:34 +0800

    feat(mcpp): bump to 0.0.29 for all platforms (#241)

--- pkgs/m/mcpp.lua
+++ pkgs/m/mcpp.lua
@@ -39,7 +39,8 @@ package = {
     xpm = {
         linux = {
             url_template = "https://github.com/mcpp-community/mcpp/releases/download/v{version}/mcpp-{version}-linux-x86_64.tar.gz",
-            ["latest"] = { ref = "0.0.28" },
+            ["latest"] = { ref = "0.0.29" },
+            ["0.0.29"] = "XLINGS_RES",
             ["0.0.28"] = "XLINGS_RES",
             ["0.0.27"] = "XLINGS_RES",
             ["0.0.26"] = "XLINGS_RES",
@@ -67,7 +68,8 @@ package = {
             ["0.0.1"] = "XLINGS_RES",
         },
         macosx = {
-            ["latest"] = { ref = "0.0.28" },
+            ["latest"] = { ref = "0.0.29" },
+            ["0.0.29"] = "XLINGS_RES",
             ["0.0.28"] = "XLINGS_RES",
             ["0.0.27"] = "XLINGS_RES",
             ["0.0.26"] = "XLINGS_RES",
@@ -81,7 +83,8 @@ package = {
             ["0.0.16"] = "XLINGS_RES",
         },
         windows = {
-            ["latest"] = { ref = "0.0.28" },
+            ["latest"] = { ref = "0.0.29" },
+            ["0.0.29"] = "XLINGS_RES",
             ["0.0.28"] = "XLINGS_RES",
             ["0.0.27"] = "XLINGS_RES",
             ["0.0.26"] = "XLINGS_RES",

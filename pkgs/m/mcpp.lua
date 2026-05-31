0f691ce23a92d16667cceb4ead6b9aeb66494199
SPeak <speakshen@163.com>
2026-05-31 20:41:20 +0800

    feat(mcpp): add 0.0.38 package
    Update mcpp latest to 0.0.38 and add xlings-res mirror entries.

--- pkgs/m/mcpp.lua
+++ pkgs/m/mcpp.lua
@@ -39,7 +39,8 @@ package = {
     xpm = {
         linux = {
             url_template = "https://github.com/mcpp-community/mcpp/releases/download/v{version}/mcpp-{version}-linux-x86_64.tar.gz",
-            ["latest"] = { ref = "0.0.37" },
+            ["latest"] = { ref = "0.0.38" },
+            ["0.0.38"] = "XLINGS_RES",
             ["0.0.37"] = "XLINGS_RES",
             ["0.0.36"] = "XLINGS_RES",
             ["0.0.35"] = "XLINGS_RES",
@@ -75,7 +76,8 @@ package = {
             ["0.0.1"] = "XLINGS_RES",
         },
         macosx = {
-            ["latest"] = { ref = "0.0.37" },
+            ["latest"] = { ref = "0.0.38" },
+            ["0.0.38"] = "XLINGS_RES",
             ["0.0.37"] = "XLINGS_RES",
             ["0.0.36"] = "XLINGS_RES",
             ["0.0.35"] = "XLINGS_RES",
@@ -97,7 +99,8 @@ package = {
             ["0.0.16"] = "XLINGS_RES",
         },
         windows = {
-            ["latest"] = { ref = "0.0.37" },
+            ["latest"] = { ref = "0.0.38" },
+            ["0.0.38"] = "XLINGS_RES",
             ["0.0.37"] = "XLINGS_RES",
             ["0.0.36"] = "XLINGS_RES",
             ["0.0.35"] = "XLINGS_RES",

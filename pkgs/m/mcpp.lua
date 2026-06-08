9331cd86d02de0c1a5710d439ecf99c031411126
SPeak <speakshen@163.com>
2026-06-09 02:44:36 +0800

    mcpp: add 0.0.53, latest -> 0.0.53 (#285)
    * mcpp: add 0.0.53, latest -> 0.0.53 (live download progress for library/component installs)
    
    * test(mcpp): bump version pins 0.0.52 -> 0.0.53

--- pkgs/m/mcpp.lua
+++ pkgs/m/mcpp.lua
@@ -39,7 +39,8 @@ package = {
     xpm = {
         linux = {
             url_template = "https://github.com/mcpp-community/mcpp/releases/download/v{version}/mcpp-{version}-linux-x86_64.tar.gz",
-            ["latest"] = { ref = "0.0.52" },
+            ["latest"] = { ref = "0.0.53" },
+            ["0.0.53"] = "XLINGS_RES",
             ["0.0.52"] = "XLINGS_RES",
             ["0.0.51"] = "XLINGS_RES",
             ["0.0.50"] = "XLINGS_RES",
@@ -87,7 +88,8 @@ package = {
             ["0.0.1"] = "XLINGS_RES",
         },
         macosx = {
-            ["latest"] = { ref = "0.0.52" },
+            ["latest"] = { ref = "0.0.53" },
+            ["0.0.53"] = "XLINGS_RES",
             ["0.0.52"] = "XLINGS_RES",
             ["0.0.51"] = "XLINGS_RES",
             ["0.0.50"] = "XLINGS_RES",
@@ -121,7 +123,8 @@ package = {
             ["0.0.16"] = "XLINGS_RES",
         },
         windows = {
-            ["latest"] = { ref = "0.0.52" },
+            ["latest"] = { ref = "0.0.53" },
+            ["0.0.53"] = "XLINGS_RES",
             ["0.0.52"] = "XLINGS_RES",
             ["0.0.51"] = "XLINGS_RES",
             ["0.0.50"] = "XLINGS_RES",

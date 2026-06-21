5c199ca374d5bb1ddd9d5852e20f89cfacb1f237
sunrisepeak <x.d2learn.org@gmail.com>
2026-06-22 04:21:43 +0800

    mcpp: register 0.0.58 (latest), XLINGS_RES mirrors for linux/macosx/windows
    Mirrors live at xlings-res/mcpp@0.0.58 (github + gitcode), byte-identical to
    mcpp-community/mcpp v0.0.58 release artifacts.

--- pkgs/m/mcpp.lua
+++ pkgs/m/mcpp.lua
@@ -39,7 +39,8 @@ package = {
     xpm = {
         linux = {
             url_template = "https://github.com/mcpp-community/mcpp/releases/download/v{version}/mcpp-{version}-linux-x86_64.tar.gz",
-            ["latest"] = { ref = "0.0.57" },
+            ["latest"] = { ref = "0.0.58" },
+            ["0.0.58"] = "XLINGS_RES",
             ["0.0.57"] = "XLINGS_RES",
             ["0.0.56"] = "XLINGS_RES",
             ["0.0.55"] = "XLINGS_RES",
@@ -92,7 +93,8 @@ package = {
             ["0.0.1"] = "XLINGS_RES",
         },
         macosx = {
-            ["latest"] = { ref = "0.0.57" },
+            ["latest"] = { ref = "0.0.58" },
+            ["0.0.58"] = "XLINGS_RES",
             ["0.0.57"] = "XLINGS_RES",
             ["0.0.56"] = "XLINGS_RES",
             ["0.0.55"] = "XLINGS_RES",
@@ -131,7 +133,8 @@ package = {
             ["0.0.16"] = "XLINGS_RES",
         },
         windows = {
-            ["latest"] = { ref = "0.0.57" },
+            ["latest"] = { ref = "0.0.58" },
+            ["0.0.58"] = "XLINGS_RES",
             ["0.0.57"] = "XLINGS_RES",
             ["0.0.56"] = "XLINGS_RES",
             ["0.0.55"] = "XLINGS_RES",

246a92cfbd6c2f865b73ed43bd44a91425d61dfa
sunrisepeak <x.d2learn.org@gmail.com>
2026-06-23 17:01:53 +0800

    feat(mcpp): add 0.0.60 (bundles xlings 0.4.58 / Termux connect+UI fixes); latest -> 0.0.60

--- pkgs/m/mcpp.lua
+++ pkgs/m/mcpp.lua
@@ -39,7 +39,8 @@ package = {
     xpm = {
         linux = {
             url_template = "https://github.com/mcpp-community/mcpp/releases/download/v{version}/mcpp-{version}-linux-x86_64.tar.gz",
-            ["latest"] = { ref = "0.0.59" },
+            ["latest"] = { ref = "0.0.60" },
+            ["0.0.60"] = "XLINGS_RES",
             ["0.0.59"] = "XLINGS_RES",
             ["0.0.58"] = "XLINGS_RES",
             ["0.0.57"] = "XLINGS_RES",
@@ -94,7 +95,8 @@ package = {
             ["0.0.1"] = "XLINGS_RES",
         },
         macosx = {
-            ["latest"] = { ref = "0.0.59" },
+            ["latest"] = { ref = "0.0.60" },
+            ["0.0.60"] = "XLINGS_RES",
             ["0.0.59"] = "XLINGS_RES",
             ["0.0.58"] = "XLINGS_RES",
             ["0.0.57"] = "XLINGS_RES",
@@ -135,7 +137,8 @@ package = {
             ["0.0.16"] = "XLINGS_RES",
         },
         windows = {
-            ["latest"] = { ref = "0.0.59" },
+            ["latest"] = { ref = "0.0.60" },
+            ["0.0.60"] = "XLINGS_RES",
             ["0.0.59"] = "XLINGS_RES",
             ["0.0.58"] = "XLINGS_RES",
             ["0.0.57"] = "XLINGS_RES",

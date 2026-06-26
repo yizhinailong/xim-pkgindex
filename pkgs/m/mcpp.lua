8ea28204fbbe65414af8cc54092e92352f0c18ff
SPeak <speakshen@163.com>
2026-06-26 22:06:59 +0800

    mcpp: 0.0.67 (identity-first candidate selection) (#321)
    Co-authored-by: sunrisepeak <x.d2learn.org@gmail.com>

--- pkgs/m/mcpp.lua
+++ pkgs/m/mcpp.lua
@@ -39,7 +39,8 @@ package = {
     xpm = {
         linux = {
             url_template = "https://github.com/mcpp-community/mcpp/releases/download/v{version}/mcpp-{version}-linux-x86_64.tar.gz",
-            ["latest"] = { ref = "0.0.66" },
+            ["latest"] = { ref = "0.0.67" },
+            ["0.0.67"] = "XLINGS_RES",
             ["0.0.66"] = "XLINGS_RES",
             ["0.0.65"] = "XLINGS_RES",
             ["0.0.64"] = "XLINGS_RES",
@@ -101,7 +102,8 @@ package = {
             ["0.0.1"] = "XLINGS_RES",
         },
         macosx = {
-            ["latest"] = { ref = "0.0.66" },
+            ["latest"] = { ref = "0.0.67" },
+            ["0.0.67"] = "XLINGS_RES",
             ["0.0.66"] = "XLINGS_RES",
             ["0.0.65"] = "XLINGS_RES",
             ["0.0.64"] = "XLINGS_RES",
@@ -149,7 +151,8 @@ package = {
             ["0.0.16"] = "XLINGS_RES",
         },
         windows = {
-            ["latest"] = { ref = "0.0.66" },
+            ["latest"] = { ref = "0.0.67" },
+            ["0.0.67"] = "XLINGS_RES",
             ["0.0.66"] = "XLINGS_RES",
             ["0.0.65"] = "XLINGS_RES",
             ["0.0.64"] = "XLINGS_RES",
